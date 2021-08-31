4ea739d98a10bf0d7307fbfec283562780e3189a
Ephraim Benson <ephraimlbenson@gmail.com>
2021-08-30 19:44:25 -0600

    removed unused black swatch

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'welcome_page/welcome_page.dart';
 import 'welcome_page/welcome_drawer.dart';
 
 void main() => runApp(MyApp());
-
-// const MaterialColor primaryBlack = MaterialColor(
-//   _blackPrimaryValue,
-//   <int, Color>{
-//     50: Color(0xFF000000),
-//     100: Color(0xFF000000),
-//     200: Color(0xFF000000),
-//     300: Color(0xFF000000),
-//     400: Color(0xFF000000),
-//     500: Color(_blackPrimaryValue),
-//     600: Color(0xFF000000),
-//     700: Color(0xFF000000),
-//     800: Color(0xFF000000),
-//     900: Color(0xFF000000),
-//   },
-// );
-// const int _blackPrimaryValue = 0xFF000000;
-// Might use later
 
 class MyApp extends StatelessWidget {
   @override
