a3e79996fe4a4ad4e1f2afd6d0b32bf077369470
Ephraim Benson <ephraim@nobleapplications.com>
2021-08-30 18:39:16 -0600

    simpler drawer

--- lib/welcome_page/welcome_drawer.dart
+++ lib/welcome_page/welcome_drawer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:nightly/welcome_page/welcome_page.dart';
 
 class WelcomeDrawer extends StatefulWidget {
@@ -8,134 +7,88 @@ class WelcomeDrawer extends StatefulWidget {
 }
 
 class _WelcomeDrawerState extends State<WelcomeDrawer> {
-  final _advancedDrawerController = AdvancedDrawerController();
+  final nightlyGif = new Image.asset('assets/images/anim_cropped.gif');
 
   @override
   Widget build(BuildContext context) {
-    return AdvancedDrawer(
-      // backdropColor: Colors.indigo[400],
-      backdropColor: Colors.grey[900],
-      controller: _advancedDrawerController,
-      animationCurve: Curves.easeInOut,
-      animationDuration: const Duration(milliseconds: 300),
-      animateChildDecoration: true,
-      rtlOpening: false,
-      disabledGestures: false,
-      childDecoration: const BoxDecoration(
-        // NOTICE: Uncomment if you want to add shadow behind the page.
-        // Keep in mind that it may cause animation jerks.
-        // boxShadow: <BoxShadow>[
-        //   BoxShadow(
-        //     color: Colors.black12,
-        //     blurRadius: 0.0,
-        //   ),
-        // ],
-        borderRadius: const BorderRadius.all(Radius.circular(16)),
-      ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: SizedBox(
-            child: Text('NIGHTLY',
-                style: TextStyle(
-                  fontSize: 45.0,
-                  fontFamily: 'RobotoMono',
-                  fontWeight: FontWeight.bold,
-                  foreground: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 2
-                    ..color = Colors.yellow,
-                )),
-          ),
-          leading: IconButton(
-            onPressed: _handleMenuButtonPressed,
-            icon: ValueListenableBuilder<AdvancedDrawerValue>(
-              valueListenable: _advancedDrawerController,
-              builder: (_, value, __) {
-                return AnimatedSwitcher(
-                  duration: Duration(milliseconds: 250),
-                  child: Icon(
-                    value.visible ? Icons.clear : Icons.menu,
-                    key: ValueKey<bool>(value.visible),
-                  ),
-                );
-              },
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'NIGHTLY',
+            style: TextStyle(
+              fontSize: 45.0,
+              fontFamily: 'RobotoMono',
+              fontWeight: FontWeight.bold,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 2
+                ..color = Colors.yellow,
             ),
-          ),
-        ),
-        body: WelcomePage(),
-      ),
-      drawer: SafeArea(
-        child: Container(
-          child: ListTileTheme(
-            textColor: Colors.yellow,
-            iconColor: Colors.yellow,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
+          )),
+      body: WelcomePage(),
+      drawer: Container(
+        width: 200,
+        child: Drawer(
+          child: Container(
+            color: Colors.black,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
                 Container(
-                  width: 128.0,
-                  height: 128.0,
-                  margin: const EdgeInsets.only(
-                    top: 24.0,
-                    bottom: 64.0,
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    color: Colors.black26,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Image.asset(
-                    'assets/images/anim_cropped.gif',
-                  ),
-                ),
-                ListTile(
-                  onTap: () {},
-                  leading: Icon(Icons.home),
-                  title: Text('Home'),
-                ),
-                ListTile(
-                  onTap: () {},
-                  leading: Icon(Icons.account_circle_rounded),
-                  title: Text('Profile'),
-                ),
-                ListTile(
-                  onTap: () {},
-                  leading: Icon(Icons.favorite),
-                  title: Text('Favorites'),
-                ),
-                ListTile(
-                  onTap: () {},
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                ),
-                Spacer(),
-                DefaultTextStyle(
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white54,
-                  ),
-                  child: Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 16.0,
+                  height: 150,
+                  child: DrawerHeader(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
                       ),
-                      child: Column(
-                        children: [
-                          Text("Terms of Service | Privacy Policy"),
-                          Text("Built with ❤️ by Nightly Team")
-                        ],
-                      )),
+                      child: nightlyGif),
                 ),
+                ListTile(
+                  leading: Icon(
+                    Icons.add,
+                    color: Colors.yellow,
+                  ),
+                  title: Text(
+                    'New Invite',
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.drafts_outlined,
+                    color: Colors.yellow,
+                  ),
+                  title: Text(
+                    'Drafts',
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.group,
+                    color: Colors.yellow,
+                  ),
+                  title: Text(
+                    'Groups',
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    color: Colors.yellow,
+                  ),
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                ),
+                //done
               ],
             ),
           ),
         ),
       ),
     );
-  }
-
-  void _handleMenuButtonPressed() {
-    // NOTICE: Manage Advanced Drawer state through the Controller.
-    // _advancedDrawerController.value = AdvancedDrawerValue.visible();
-    _advancedDrawerController.showDrawer();
   }
 }
